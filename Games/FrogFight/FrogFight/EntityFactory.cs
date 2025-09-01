8532af694cc309341a6624129914c0d78880b733
Johan Wångsell <johan.wangsell@surgicalscience.com>
2025-09-01 17:43:57 +0200

    animation stuff

--- Games/FrogFight/FrogFight/EntityFactory.cs
+++ Games/FrogFight/FrogFight/EntityFactory.cs
@@ -20,6 +20,7 @@ using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using FrogFight.Physics;
+using JapeFramework.Aseprite;
 using JapeFramework.JsonClasses;
 using MonoGame.Extended.Serialization.Json;
 using Newtonsoft.Json;
@@ -42,57 +43,16 @@ namespace FrogFight
       m_graphicsDevice = graphicsDevice;
     }
 
+
+
     public Entity CreatePlayer(Vector2 position, int playerNumber, int networkHandle, GGPOPlayer? networkPlayerInfo, bool isLocal)
     {
       var entity = m_ecsWorld.CreateEntity();
 
-      var img = AssetManager.Load<Texture2D>(ContentDirectory.Textures.Game.Frog.frog_animations_combined_png);
+      var animSprite = AsepriteHelper.LoadAnim(ContentDirectory.Textures.Game.Frog.frog_animations_combined_png, 
+                                ContentDirectory.Textures.Game.Frog.frog_animations_combined_json,
+                                "lick");
 
-      var jsonText = AssetManager.Load<string>(ContentDirectory.Textures.Game.Frog.frog_animations_combined_json);
-      var json = JsonConvert.DeserializeObject<Root>(jsonText);
-
-      var framesObject = JObject.Parse(json.Frames.ToString());
-
-      List<Root2> frames = new List<Root2>();
-
-      foreach (var framePair in framesObject)
-      {
-        var key = framePair.Key;
-        var val = framePair.Value;
-
-        var frameData = JsonConvert.DeserializeObject<Root2>(val.ToString());
-        frames.Add(frameData);
-      }
-
-      var fileName = Path.GetFileNameWithoutExtension(json.Meta.Image);
-
-      var dudeAtlas = Texture2DAtlas.Create($"TextureAtlas//{fileName}", img, frames[0].Frame.W, frames[0].Frame.H);
-      var spriteSheet = new SpriteSheet($"SpriteSheet//{fileName}", dudeAtlas);
-
-      foreach (var frameTag in json.Meta.FrameTags)
-      {
-        var animName = frameTag.Name;
-        var from = frameTag.From;
-        var to = frameTag.To;
-
-        for (int i = from; i < to; i++)
-        {
-          dudeAtlas.CreateRegion(frames[i].Frame.X, frames[i].Frame.Y, frames[i].Frame.W, frames[i].Frame.H, animName + i);
-        }
-
-        spriteSheet.DefineAnimation(animName, builder =>
-        {
-          builder.IsLooping(true);
-          for (int i = from; i < to; i++)
-          {
-            builder.AddFrame(animName + i, TimeSpan.FromMilliseconds(frames[i].Duration));
-          }
-        });
-      }
-
-      var animSprite = new AnimatedSprite(spriteSheet, "lick");
-      //animSprite.Origin = new Vector2(11 / 2.0f, 9 / 2.0f);
-      animSprite.Origin = new Vector2(frames[0].Frame.W / 2.0f, frames[0].Frame.H / 2.0f);
       entity.Attach(animSprite);
 
       entity.Attach(new Transform2(position, 0, Vector2.One));
---
+++ JapeFramework/Aseprite/AsepriteHelper.cs
@@ -0,0 +1,69 @@
+﻿using AsyncContent;
+using JapeFramework.JsonClasses;
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended.Graphics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace JapeFramework.Aseprite
+{
+  public class AsepriteHelper
+  {
+    public static AnimatedSprite LoadAnim(string pngPath, string jsonPath, string initialAnim)
+    {
+      var img = AssetManager.Load<Texture2D>(pngPath);
+      var jsonText = AssetManager.Load<string>(jsonPath);
+
+      var json = JsonConvert.DeserializeObject<Root>(jsonText);
+
+      var framesObject = JObject.Parse(json.Frames.ToString());
+
+      List<Root2> frames = new List<Root2>();
+
+      foreach (var framePair in framesObject)
+      {
+        var key = framePair.Key;
+        var val = framePair.Value;
+
+        var frameData = JsonConvert.DeserializeObject<Root2>(val.ToString());
+        frames.Add(frameData);
+      }
+
+      var fileName = Path.GetFileNameWithoutExtension(json.Meta.Image);
+
+      var dudeAtlas = Texture2DAtlas.Create($"TextureAtlas//{fileName}", img, frames[0].Frame.W, frames[0].Frame.H);
+      var spriteSheet = new SpriteSheet($"SpriteSheet//{fileName}", dudeAtlas);
+
+      foreach (var frameTag in json.Meta.FrameTags)
+      {
+        var animName = frameTag.Name;
+        var from = frameTag.From;
+        var to = frameTag.To;
+
+        for (int i = from; i < to; i++)
+        {
+          dudeAtlas.CreateRegion(frames[i].Frame.X, frames[i].Frame.Y, frames[i].Frame.W, frames[i].Frame.H, animName + i);
+        }
+
+        spriteSheet.DefineAnimation(animName, builder =>
+        {
+          builder.IsLooping(true);
+          for (int i = from; i < to; i++)
+          {
+            builder.AddFrame(animName + i, TimeSpan.FromMilliseconds(frames[i].Duration));
+          }
+        });
+      }
+
+      var animSprite = new AnimatedSprite(spriteSheet, initialAnim);
+      animSprite.Origin = new Vector2(frames[0].Frame.W / 2.0f, frames[0].Frame.H / 2.0f);
+      return animSprite;
+    }
+  }
+}
