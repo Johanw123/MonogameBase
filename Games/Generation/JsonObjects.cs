494bbb2c8c9b21c9f37831c468c100224dab38a5
Johan Wångsell <johan.wangsell@surgicalscience.com>
2026-07-26 23:46:28 +0200

    stuff

--- Games/Generation/JsonObjects.cs
+++ Games/Generation/JsonObjects.cs
@@ -56,6 +56,9 @@ public class JsonButton
   [JsonPropertyName("lockedindemo")]
   public string LockedInDemo { get; set; } = "false";
 
+  [JsonPropertyName("tooltippercentage")]
+  public string TooltipShowPercentage { get; set; } = "true";
+
   [JsonPropertyName("buttonsizescale")]
   public string ButtonSizeScale { get; set; } = "1.0";
 }
